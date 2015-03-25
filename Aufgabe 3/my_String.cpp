8859508db5c2db731585b35fb6b372f0b4c2c5b1
Tobias Kerst <tobias.kerst@tobscore.com>
2015-03-25 15:47:55 +0100

    Improves testing
    This commit improves the testing output by exporting the "PASSED" and "FAILED" message to different functions. Also the testing environment now prints the parts, it is trying to test, instead of just displaying, whether an error occured.

--- Aufgabe 3/my_String.cpp
+++ Aufgabe 3/my_String.cpp
@@ -68,7 +68,10 @@ ostream& operator<< (ostream &out, String &s) {
 void testEmpty();
 void testSingleChar();
 void testCharPointer();
+void testCount();
 void testStringObject();
+void passedMessage();
+void failedMessage();
 
 
 // Test the class in the main method by calling each corresponding test.
@@ -76,6 +79,7 @@ int main() {
     testEmpty();
     testSingleChar();
     testCharPointer();
+    testCount();
     testStringObject();
 }
 
@@ -90,6 +94,8 @@ void testEmpty() {
     cout << "##Test simple String initialization" << endl;
     
     String* s1 = new String();
+    
+    cout << "Check if string could be created: ";
     if(s1 != NULL) {
         cout << "PASSED" << endl;
     } else {
@@ -102,16 +108,18 @@ void testSingleChar() {
     cout << "\n\n##Test String Initialization with single char" << endl;
     String s1('H'), s2('t');
     
+    cout << "Check if one-character String is initialized correctly: ";
     if((*s1.getString()) == 'H')
-        cout << "PASSED" << endl;
+        passedMessage();
     else {
-        cout << "FAILED" << endl;
+        failedMessage();
     }
     
+    cout << "Check for lower-case letters: ";
     if((*s2.getString()) == 't')
-        cout << "PASSED" << endl;
+        passedMessage();
     else {
-        cout << "FAILED" << endl;
+        failedMessage();
     }
 }
 
@@ -124,25 +132,50 @@ void testCharPointer() {
     char c3[] = "Hello World";
     String s1(c1), s2(c2), s3(c3);
     
+    cout << "Compare newly created String with original char-array: ";
     if(strcmp(s1.getString(), "Hello") == 0) {
-        cout << "PASSED" << endl;
+        passedMessage();
     } else {
-        cout << "FAILED" << endl;
+        failedMessage();
     }
     
+    cout << "Test space seperated strings: ";
     if(strcmp(s3.getString(), "Hello World") == 0) {
-        cout << "PASSED" << endl;
+        passedMessage();
     } else {
-        cout << "FAILED" << endl;
+        failedMessage();
     }
     
+    cout << "Test newly created string with original string: ";
     if(strcmp(s3.getString(), c3) == 0) {
-        cout << "PASSED" << endl;
+        passedMessage();
     } else {
-        cout << "FAILED" << endl;
+        failedMessage();
     }
     
-    cout << "Print s1 and s2: " << s1 << " " << s2 << endl;
+}
+
+void testCount() {
+    cout << "\n\n##Test Size of Strings" << endl;
+    String s1('H');
+    
+    cout << "Check String length (1 Character): ";
+    if(s1.getSize() == 1) {
+        passedMessage();
+    } else {
+        failedMessage();
+    }
+    
+    char c2[] = "Hello";
+    char c3[] = "World";
+    String s2(c2), s3(c3);
+    
+    cout << "Compare to different strings with same size: ";
+    if(s2.getSize() == s3.getSize()) {
+        passedMessage();
+    } else {
+        failedMessage();
+    }
 }
 
 void testStringObject() {
@@ -151,10 +184,26 @@ void testStringObject() {
     char c[] = "Hello World";
     String s1(c);
     String s2(s1);
-    if(strcmp(s1.getString(), s2.getString()) == 0) {
-        cout << "PASSED" << endl;
+    
+    cout << "Check if copied String has the same size as the original one: ";
+    if(s2.getSize() == s1.getSize()) {
+        passedMessage();
     } else {
-        cout << "FAILED" << endl;
+        failedMessage();
     }
     
+    if(strcmp(s2.getString(), "Hello World") == 0 ) {
+        passedMessage();
+    } else {
+        failedMessage();
+    }
+}
+
+
+void passedMessage() {
+    cout << "PASSED" << endl;
+}
+
+void failedMessage() {
+    cout << "FAILED" << endl;
 }
