ca0d2087f3486ed2f1c968c9dbb845dfb5c47dfd
Tobias Kerst <tobias.kerst@tobscore.com>
2015-03-30 16:10:27 +0200

    Fixes spacing and adds functionality
    The functionality that this commti introduces is comparable to an array index feature, where you can get a character, by using array syntax, and in addition to that, you can assign one string to another, by simply using the '=' operator.

--- Aufgabe 3/my_String.cpp
+++ Aufgabe 3/my_String.cpp
@@ -18,12 +18,14 @@ String::String() {
     str = new char[1];
     str[0] = '\0';
 }
+
 String::String(char c) {
     size = 1;
     str = new char[2];
     str[0] = c;
     str[1] = '\0';
 }
+
 String::String(const char* c) {
     size = 0;
     // Get the length of the passed string and set the object's parameter accordingly
@@ -38,6 +40,7 @@ String::String(const char* c) {
         str[i] = c[i];
     }
 }
+
 String::String(const String& copyConstructor) { // This is my copy constructor
     
     size = copyConstructor.size;
@@ -46,12 +49,42 @@ String::String(const String& copyConstructor) { // This is my copy constructor
         str[i] = (char) copyConstructor.str[i];
     }
 }
+
 String::~String() {
     delete[] str;
 }
+
 int String::getSize() {
     return size;
 }
+
 char* String::getString() {
     return str;
+}
+
+// Points to the char at the given position, by simply adding the index to the adress (simple pointer arithmetic)
+char& String::operator[](int index) {
+
+    if(index < size && index >= 0) {
+        return *(str + index);
+    }
+    
+    // If the index is out of bounds, the point to the null-terminator
+    return *(str + size);
+}
+
+String& String::operator=(String& s) {
+    // free old string's memory
+    delete[] str;
+    
+    // Create new copied String
+    size = s.getSize();
+    str = new char[size];
+    for(int i = 0; i < size; i++) {
+        str[i] = s[i];
+    }
+    return *this;
+}
+
+    return *this;
 }
